4abe09d14689f18d075e5cfa5f19db7759ef6b8f
TsymbalyukM <mashatsymbaliuk2@gmail.com>
2023-07-05 00:12:15 +0300

    add api

---
+++ src/components/API/Api.jsx
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+
+const API_KEY = '36783014-3ac2be553b32ec8827c640700';
+export const perPage = 12;
+
+export const pixabayImages = async (query, page) => {
+  const response = await axios.get(
+    `?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
+  );
+  return response.data;
+};
+export const normalizedImages = imagesArray =>
+  imagesArray.map(({ id, tags, webformatURL, largeImageURL }) => {
+    return { id, tags, webformatURL, largeImageURL };
+  });
